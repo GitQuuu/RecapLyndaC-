ad595afa819752f927742542dc0cc7f8efeb3150
Qu <Quanvle@live.dk>
2019-05-29 10:15:12 +0200

    Challenge Multiple Choice Questions 100%

--- RecapLyndaC-/Program.cs
+++ RecapLyndaC-/Program.cs
@@ -8,9 +8,31 @@ namespace RecapLyndaC_
 {
     class Program
     {
+    
         static void Main(string[] args)
         {
-            Console.WriteLine("Testing if the setup is working");
+            
+            ///Multiple Choice question Task
+            ///Create a simple three-question survey
+            ///1. what is your name 2. How old are you 3.What month were you born in
+            ///Output the answers back into the console
+
+            string name;
+            int age;
+            string birthMonth;
+        
+            Console.WriteLine("What is your name?");
+            name = Console.ReadLine();
+            Console.WriteLine("How old are you?");
+            age = int.Parse(Console.ReadLine());
+            Console.WriteLine("What month were you born in?");
+            birthMonth = Console.ReadLine();
+            
+            Console.WriteLine($"\nAccording to your indication, Name is:{name} Age is:{age} Birth of month is:{birthMonth}");
+
+
+            Console.ReadLine();
+
         }
     }
 }

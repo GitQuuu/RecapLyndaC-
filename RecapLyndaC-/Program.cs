e6e196928060d0398c885b10783c4011444a20c9
Qu <Quanvle@live.dk>
2019-06-03 14:18:51 +0200

    Survey OOP challange 100%
    task completede and added a function that takes in a question and a readline and returns a readline

--- RecapLyndaC-/Program.cs
+++ RecapLyndaC-/Program.cs
@@ -16,14 +16,11 @@ namespace RecapLyndaC_
             ///Use a class to store survey data use OOP 
 
             Data Data = new Data();
-        
-            Console.WriteLine("What is your name?");
-            Data.Name = Console.ReadLine();
-            Console.WriteLine("How old are you?");
-            Data.Age = int.Parse(Console.ReadLine());
-            Console.WriteLine("What month were you born in?");
-            Data.BirthMonth = Console.ReadLine();
-            
+
+            Data.Name = Util.Ask("What is your name?");
+            Data.Age = int.Parse(Util.Ask("How old are you?"));
+            Data.BirthMonth = Util.Ask("What month were you born in?");
+    
             Console.WriteLine($"\nAccording to your indication, Name is:{Data.Name} Age is:{Data.Age} Birth of month is:{Data.BirthMonth}");
 
 

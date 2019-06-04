ccefc339b4c0587ded94df3675b224e13b6e655e
Qu <Quanvle@live.dk>
2019-06-04 09:42:46 +0200

    Survey OOP challenge add a failsafe
    force age variabel to int, with a  do while and if statement

--- RecapLyndaC-/Program.cs
+++ RecapLyndaC-/Program.cs
@@ -19,22 +19,40 @@ namespace RecapLyndaC_
 
             Data.Name = Util.Ask("What is your name?");
 
-            Console.WriteLine("How old are you");
             bool forceAgetoInt = true;
-            while (forceAgetoInt)
+            do
             {
-                if (int.TryParse(Console.ReadLine(), out int age))
+               if (int.TryParse(Util.Ask("What is your age?"), out int result))
                 {
-                    Data.Age = age;
+                    result = Data.Age;
                     forceAgetoInt = false;
                 }
                 else
                 {
-                    Console.WriteLine("value must be a number, try again");
+                    Console.WriteLine("Value must be a number, please try again");
                 }
-                
-            }
+              
+            } while (forceAgetoInt);
+
             
+            /// Does the same for line 22-35 just a different way to approch the result 
+            ///Console.WriteLine("what is your age");
+            ///bool forceAgetoInt = true;
+            ///while (forceAgetoInt)
+            ///{
+            ///    if (int.TryParse(Console.ReadLine(), out int age))
+            ///    {
+            ///        Data.Age = age;
+            ///        forceAgetoInt = false;
+            ///    }
+            ///    else
+            ///    {
+            ///        Console.WriteLine("value must be a number, try again");
+            ///    }
+            ///
+            ///}
+
+
             Data.BirthMonth = Util.Ask("What month were you born in?");
     
             Console.WriteLine($"\nAccording to your inputs, Name is:{Data.Name} Age is:{Data.Age} Birth of month is:{Data.BirthMonth}");

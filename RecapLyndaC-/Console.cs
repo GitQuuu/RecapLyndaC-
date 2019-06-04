c0726329789284d0aa859d99d5cc60ea864d6994
Qu <Quanvle@live.dk>
2019-06-04 08:26:41 +0200

    Reverting back to working version of Survey OOP 100%

---
+++ RecapLyndaC-/Console.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util
+{
+    class Console
+    {
+        /// <summary>
+        /// Creating a class name Console to force an error from our main program class , this will conflict our Console.WriteLine, because our main is trying to find 
+        /// a Console.WriteLine inside our Console class which doesnt excist, we could simply rename our class to something else, but we do this to show that its a namespace
+        /// issue and can be prevented by renameing our namespace to something like Util
+        /// </summary>
+        /// <param name="question"></param>
+        /// <returns></returns>
+
+
+        /// we wanna create a function that ask a question and takes in a Console.ReadLine(); since we do that alot
+        /// 
+        static public string Ask(string question)
+        {
+            System.Console.WriteLine(question);
+            return System.Console.ReadLine();
+        }
+
+
+    }
+}
---
+++ RecapLyndaC-/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecapLyndaC_
+{
+    class Student:Member
+    {
+        static public int Count = 0;
+
+        protected int grade;
+     
+        public int Grade { get => grade; set => grade = value; }        
+
+        public Student()
+        {
+
+        }
+
+        public Student( string name, int grade, string birthday, string address, int phone)
+        {
+            Grade = grade;
+            Name = name;
+            Birthday = birthday;
+            Address = address;
+            Phone = phone;
+        }
+
+    }
+}
